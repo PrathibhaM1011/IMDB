f1603dfb8021ab8ff4ddfae363e82a8b0b8c5392
PrathibhaM1011 <prathibhamamindla2910@gmail.com>
2025-03-21 11:02:38 +0530

    Update imdb.js

--- New folder/imdb.js
+++ New folder/imdb.js
@@ -1,5 +1,8 @@
 let container = document.getElementById("container");
 const url = 'https://iris-spice-uncle.glitch.me/result';
+let loader = document.getElementById("loader-container");
+
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -18,16 +21,19 @@ try {
         throw new Error("Invalid or failed to fetch", response.status);
     }
 	const result = await response.json();
-	displayData(result);
-} catch (err) {
-	console.error(err);
-}}
+	setTimeout(() => {
+        displayData(result);
+    }, 4000); // 4 seconds delay
 
+} catch (err) {
+    console.error(err.message);
+}}
 
 
  function displayData(obj){
     console.log(obj)
-obj.forEach(element => {
+    container.innerHTML = ""; 
+obj.forEach((element, index) => {
       let item =   document.createElement("div");
       item.className = "item"    
       item.innerHTML= `
@@ -36,11 +42,13 @@ obj.forEach(element => {
       <p>IMDB rating: ${element.IMDB_Rating}</p>
       <img src="${element.Poster_Link}">
 
-      `
+      `;
+      item.style.animationDelay = `${index * 0.1}s`;
+      
       container.appendChild(item)
      
     }); 
-    
+    loader.remove();
 }
 
 window.addEventListener("DOMContentLoaded", function(){
